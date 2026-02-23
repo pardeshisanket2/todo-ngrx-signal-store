cae737deb5d67e8af13f644e24969d53006ef3a3
teknas <pardeshisanket2@gmail.com>
2026-02-23 19:45:37 +0530

    Updating production env

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   server: 'Todo_Production',
   version: '1.0.0-live',
-  hostAPI: 'http://localhost:3000', // Change to production API endpoint later
+  hostAPI: 'https://todo-ngrx-signal-store-be.netlify.app/', // Change to production API endpoint later
 };
